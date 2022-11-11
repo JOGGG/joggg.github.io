12262df86431b1faea2caf61c20e47e198eb7e6f
Jo Yang <joyang@datagile.cn>
2022-11-11 12:19:31 +0800

    。

--- Samples/FootBarGobal/FootBarGobal.js
+++ Samples/FootBarGobal/FootBarGobal.js
@@ -2,32 +2,40 @@
 
 tableau.extensions.initializeAsync().then(function () {
     //Add your JavaScript code here to call the Extensions API
-}); function addFilter() {
-    tableau.extensions.initializeAsync().then(function () {
-        // Initialization succeeded! Get the dashboard
-        
-        tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图").getDataSourcesAsync().then(datasources => {
-           var dataSource = datasources.find(datasource => datasource.name === "宏观航运New3.0");
-            return dataSource.getUnderlyingDataAsync();
-        }).then(dataTable => {
-            let field = dataTable.columns.find(column => column.fieldName === "Sup Area");
-            let list = [];
-            for (let row of dataTable.data) {
-                list.push(row[field.index].value);
-            }
-            let values = list.filter((el, i, arr) => arr.indexOf(el) === i);
-            console.log(values)
-        });
-
-        // data.getSummaryDataAsync().then(function(sumdata) {
-        //     //获取表单数据
-        //     const worksheetData = sumdata;
-        //     console.log(worksheetData);
-        // });
-        // var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
-        // data.applyFilterAsync("位置", ["广州枝山仓库"], "replace", { isExcludeMode: false })
-    }, function (err) {
-        // something went wrong in initialization
-        document.getElementById("resultBox").innerHTML = "Error while Initializing: " + err.toString();
-    });
+});
+function normal(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("status (DWS Vesselinfo)", [1], that.checked ? "add" : "remove", { isExcludeMode: false })
+    console.log('status (DWS Vesselinfo)=>', 1)
 }
+function delay(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("status (DWS Vesselinfo)", [2], that.checked ? "add" : "remove", { isExcludeMode: false })
+    console.log('status (DWS Vesselinfo)=>', 2)
+}
+function cancelled(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("status (DWS Vesselinfo)", [3], that.checked ? "add" : "remove", { isExcludeMode: false })
+    console.log('status (DWS Vesselinfo)=>', 2)
+}
+function incoming(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("Ship direction", [1], that.checked ? "add" : "remove", { isExcludeMode: false })
+    console.log('Ship direction=>', 1)
+}
+function ports(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("Type (Placeinfo)", [5], that.checked ? "add" : "remove", { isExcludeMode: false })
+    console.log('Type (Placeinfo)=>', 1)
+}
+function warehouse(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("Type (Placeinfo)", [3], that.checked ? "add" : "remove", { isExcludeMode: false })
+    console.log('Type (Placeinfo)=>', 1)
+}
+function factory(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("Type (Placeinfo)", [10], that.checked ? "add" : "remove", { isExcludeMode: false })
+    console.log('Type (Placeinfo)=>', 1)
+}
+
--- Samples/HeadBarGobal/HeadBarGobal.js
+++ Samples/HeadBarGobal/HeadBarGobal.js
@@ -1,22 +1,76 @@
 'use strict';
 
 tableau.extensions.initializeAsync().then(function () {
-    // Initialization succeeded! 
-    //Add your JavaScript code here to call the Extensions API
-}); function initialize() {
-    tableau.extensions.initializeAsync().then(function () {
-        // Initialization succeeded! Get the dashboard
-        var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图").getDataSourcesAsync().then(datasources => {
-                console.log(datasources)
-            }).then(dataTable => {
-                // process the dataTable...
-            });
-
-        // var map = dashboard.getDashboardObjectById(3)
-
-        // document.getElementById("headTitle").innerHTML = "I'm running in a dashboard named <strong>" + dashboard.name + "</strong>";
-    }, function (err) {
-        // something went wrong in initialization
-        document.getElementById("resultBox").innerHTML = "Error while Initializing: " + err.toString();
+    tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图").getDataSourcesAsync().then(datasources => {
+        var dataSource = datasources.find(datasource => datasource.name === "宏观航运New3.0");
+        return dataSource.getLogicalTablesAsync().then((logicalTables) => {
+            console.log('nihao=>', logicalTables)
+            return dataSource.getLogicalTableDataAsync(logicalTables[3].id)//基础信息表
+        });
+    }).then(dataTable => {
+        //筛选出地区
+        let field = dataTable.columns.find(column => column.fieldName === "Sup Area");
+        let list = [];
+        for (let row of dataTable.data) {
+            list.push(row[field.index].value);
+        }
+        let values = list.filter((el, i, arr) => arr.indexOf(el) === i);
+        values.forEach(item => {
+            if (item) {
+                var tar = document.getElementById("Target")
+                tar.options.add(new Option(item), item)
+            }
+        })
     });
+
+    tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图").getDataSourcesAsync().then(datasources => {
+        var dataSource = datasources.find(datasource => datasource.name === "宏观航运New3.0");
+        return dataSource.getLogicalTablesAsync().then((logicalTables) => {
+            console.log('nihao=>', logicalTables)
+            return dataSource.getLogicalTableDataAsync(logicalTables[2].id)//航线表
+        });
+    }).then(dataTable => {
+        //筛选出船名
+        let fieldA = dataTable.columns.find(column => column.fieldName === "Vessel Name");
+        let listA = [];
+        for (let row of dataTable.data) {
+            listA.push(row[fieldA.index].value);
+        }
+        let valuesA = listA.filter((el, i, arr) => arr.indexOf(el) === i);
+        valuesA.forEach(item => {
+            if (item) {
+                var shipOp = document.getElementById("Ship")
+                shipOp.options.add(new Option(item), item)
+            }
+        })
+
+        //筛选出航线
+        let fieldB = dataTable.columns.find(column => column.fieldName === "service (DWS Vesselinfo)");
+        let listB = [];
+        for (let row of dataTable.data) {
+            listB.push(row[fieldB.index].value);
+        }
+        let valuesB = listB.filter((el, i, arr) => arr.indexOf(el) === i);
+        valuesB.forEach(item => {
+            if (item) {
+                var ser = document.getElementById("Service")
+                ser.options.add(new Option(item), item)
+            }
+        })
+    });
+});
+function tarchange(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("Sup Area", [that.value], "replace", { isExcludeMode: false })
+    console.log('area change=>', that.value)
+}
+function serchange(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("service (DWS Vesselinfo)", [that.value], "replace", { isExcludeMode: false })
+    console.log('service change=>', that.value)
+}
+function shipchange(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "世界地图")
+    data.applyFilterAsync("Vessel Name", [that.value], "replace", { isExcludeMode: false })
+    console.log('ship change=>', that.value)
 }
