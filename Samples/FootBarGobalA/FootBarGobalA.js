58891d3693fd4c51231de41ea6846d02ce6d0fc2
Jo Yang <joyang@datagile.cn>
2022-11-15 22:50:53 +0800

    默认勾选，模糊搜索

--- Samples/FootBarGobalA/FootBarGobalA.js
+++ Samples/FootBarGobalA/FootBarGobalA.js
@@ -2,40 +2,91 @@
 
 tableau.extensions.initializeAsync().then(function () {
     //Add your JavaScript code here to call the Extensions API
+    var dataList =['warehouse','ports','factory']
+    dataList.forEach(item=>{
+        document.getElementById(item).checked = true
+    })
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
+    data.applyFilterAsync("类型 (海外工厂)", [10, 'Null'],  "add", { isExcludeMode: false })
+    data.applyFilterAsync("类型", [1, 'Null'], "add", { isExcludeMode: false })
+    data.applyFilterAsync("类型 (港口)", [5, 'Null'], "add", { isExcludeMode: false })
+
 });
 function normal(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("poleta_status", [1,'Null'], that.checked ? "add" : "remove", { isExcludeMode: false })
+    var filterData
+    if (that.checked) {
+        filterData = [1, 'Null']
+    } else {
+        filterData = ['Null']
+    }
+    data.applyFilterAsync("poleta_status", filterData, "add" , { isExcludeMode: false })
     console.log('poleta_status=>', 1)
 }
 function delay(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("poleta_status", [2,'Null'], that.checked ? "add" : "remove", { isExcludeMode: false })
+    var filterData
+    if (that.checked) {
+        filterData = [2, 'Null']
+    } else {
+        filterData = ['Null']
+    }
+    data.applyFilterAsync("poleta_status", filterData,"add", { isExcludeMode: false })
     console.log('poleta_status=>', 2)
 }
 function cancelled(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("poleta_status", [3,'Null'], that.checked ? "add" : "remove", { isExcludeMode: false })
-    console.log('poleta_status=>', 2)
+    var filterData
+    if (that.checked) {
+        filterData = [3, 'Null']
+    } else {
+        filterData = ['Null']
+    }
+    data.applyFilterAsync("poleta_status", filterData, "add", { isExcludeMode: false })
+    console.log('poleta_status=>', 3)
 }
 function incoming(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("Ship_direction", ['E','Null'], that.checked ? "add" : "remove", { isExcludeMode: false })
+    var filterData
+    if (that.checked) {
+        filterData = ['E', 'Null']
+    } else {
+        filterData = ['Null']
+    }
+    data.applyFilterAsync("Ship_direction", filterData, "add", { isExcludeMode: false })
     console.log('Ship_direction=>', 'E')
 }
 function ports(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("类型 (港口)", [5,'Null'], that.checked ? "add" : "remove", { isExcludeMode: false })
+    var filterData
+    if (that.checked) {
+        filterData = [5, 'Null']
+    } else {
+        filterData = ['Null']
+    }
+    data.applyFilterAsync("类型 (港口)", filterData, "add", { isExcludeMode: false })
     console.log('类型 (港口)=>', 5)
 }
 function warehouse(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("类型", [3,'Null'], that.checked ? "add" : "remove", { isExcludeMode: false })
-    console.log('类型=>', 3)
+    var filterData
+    if (that.checked) {
+        filterData = [1, 'Null']
+    } else {
+        filterData = ['Null']
+    }
+    data.applyFilterAsync("类型", filterData, "add", { isExcludeMode: false })
+    console.log('类型=>',1)
 }
 function factory(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("类型 (海外工厂)", [10,'Null'], that.checked ? "add" : "remove", { isExcludeMode: false })
+    var filterData
+    if (that.checked) {
+        filterData = [10, 'Null']
+    } else {
+        filterData = ['Null']
+    }
+    data.applyFilterAsync("类型 (海外工厂)", filterData,  "add", { isExcludeMode: false })
     console.log('类型 (海外工厂)=>', 10)
 }
 
--- Samples/HeadBarGobalA/HeadBarGobalA.js
+++ Samples/HeadBarGobalA/HeadBarGobalA.js
@@ -16,7 +16,9 @@ tableau.extensions.initializeAsync().then(function () {
             listA.push(row[fieldA.index].value);
         }
         let valuesA = listA.filter((el, i, arr) => arr.indexOf(el) === i);
+        document.getElementById("Ship").options.add(new Option('All'), 'All')
         valuesA.forEach(item => {
+            //存在筛选项
             if (item) {
                 var shipOp = document.getElementById("Ship")
                 shipOp.options.add(new Option(item), item)
@@ -30,6 +32,7 @@ tableau.extensions.initializeAsync().then(function () {
             listB.push(row[fieldB.index].value);
         }
         let valuesB = listB.filter((el, i, arr) => arr.indexOf(el) === i);
+        document.getElementById("Service").options.add(new Option('All'), 'All')
         valuesB.forEach(item => {
             if (item) {
                 var ser = document.getElementById("Service")
@@ -44,26 +47,52 @@ tableau.extensions.initializeAsync().then(function () {
             listC.push(row[fieldC.index].value);
         }
         let values = listC.filter((el, i, arr) => arr.indexOf(el) === i);
+        document.getElementById("Target").options.add(new Option('All'), 'All')
         values.forEach(item => {
             if (item) {
                 var tar = document.getElementById("Target")
                 tar.options.add(new Option(item), item)
             }
         })
+       
     });
+    shipchange({value:'All'})
+    serchange({value:'All'})
+    tarchange({value:'All'})
 });
+
+
 function tarchange(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("Region", [that.value,'Null'], "replace", { isExcludeMode: false })
-    console.log('Region change=>', that.value)
+    if (that.value === 'All') {
+        data.applyFilterAsync("Region", ['Null'], "replace", { isExcludeMode: false })
+        console.log('Region change=>', 'Null')
+    } else {
+        data.applyFilterAsync("Region", [that.value, 'Null'], "replace", { isExcludeMode: false })
+        console.log('Region change=>', that.value)
+    }
+
 }
 function serchange(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("service", [that.value,'Null'], "replace", { isExcludeMode: false })
-    console.log('service change=>', that.value)
+    if (that.value === 'All') {
+        data.applyFilterAsync("service", ['Null'], "replace", { isExcludeMode: false })
+        console.log('service change=>', 'Null')
+    } else {
+        data.applyFilterAsync("service", [that.value, 'Null'], "replace", { isExcludeMode: false })
+        console.log('service change=>', that.value)
+    }
 }
+
 function shipchange(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
-    data.applyFilterAsync("Vessel（船名）", [that.value,'Null'], "replace", { isExcludeMode: false })
+    if(that.value==='All'){
+        data.applyFilterAsync("Vessel（船名）", [ 'Null'], "replace", { isExcludeMode: false })
+        console.log('ship change=>', 'Null')
+    }else{
+        data.applyFilterAsync("Vessel（船名）", [that.value, 'Null'], "replace", { isExcludeMode: false })
     console.log('ship change=>', that.value)
+    }
+    
 }
+
