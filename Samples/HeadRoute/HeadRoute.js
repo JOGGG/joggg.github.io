28ab3b40eeecee2c4405453a8211edb8b1f34c23
Jo Yang <joyang@datagile.cn>
2022-11-16 11:48:21 +0800

    切换栏

---
+++ Samples/HeadRoute/HeadRoute.js
@@ -0,0 +1,12 @@
+'use strict';
+
+tableau.extensions.initializeAsync().then(function () {
+    
+});
+function goChina(){
+    window.parent.frames.location.href="https://10.192.112.83/#/views/KPI3_0/sheet6_1"
+}
+
+
+
+
