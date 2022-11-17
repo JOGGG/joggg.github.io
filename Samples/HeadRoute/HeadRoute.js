63c92b0ca8e040aa922a45fb1a14e32add03edd2
Jo Yang <joyang@datagile.cn>
2022-11-17 18:57:44 +0800

    up to date

--- Samples/HeadRoute/HeadRoute.js
+++ Samples/HeadRoute/HeadRoute.js
@@ -1,7 +1,9 @@
 'use strict';
 
 tableau.extensions.initializeAsync().then(function () {
-    
+    var timeDate = new Date().toLocaleDateString()
+    document.getElementById('timeA').innerHTML ='Up to date:'+ timeDate.replace(/\//g, '-')
+   
 });
 function goChina(){
     window.parent.frames.location.href="https://10.192.112.83/#/views/KPI3_0/sheet6_1"
