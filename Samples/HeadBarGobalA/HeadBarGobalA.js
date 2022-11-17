88daa82aff4b968da088a41f240b45e4bf04585e
Jo Yang <joyang@datagile.cn>
2022-11-17 11:26:34 +0800

    。

--- Samples/HeadBarGobalA/HeadBarGobalA.js
+++ Samples/HeadBarGobalA/HeadBarGobalA.js
@@ -46,17 +46,33 @@ tableau.extensions.initializeAsync().then(function () {
         for (let row of dataTable.data) {
             listC.push(row[fieldC.index].value);
         }
-        let values = listC.filter((el, i, arr) => arr.indexOf(el) === i);
+        let valuesC = listC.filter((el, i, arr) => arr.indexOf(el) === i);
         document.getElementById("Target").options.add(new Option('All'), 'All')
-        values.forEach(item => {
+        valuesC.forEach(item => {
             if (item) {
                 var tar = document.getElementById("Target")
                 tar.options.add(new Option(item), item)
             }
         })
+
+         //筛选出week
+         let fieldD = dataTable.columns.find(column => column.fieldName === "etd_weeks");
+         let listD = [];
+         for (let row of dataTable.data) {
+             listD.push(row[fieldD.index].value);
+         }
+         let valuesD = listD.filter((el, i, arr) => arr.indexOf(el) === i);
+         document.getElementById("Week").options.add(new Option('All'), 'All')
+         valuesD.forEach(item => {
+             if (item) {
+                 var tar = document.getElementById("Week")
+                 tar.options.add(new Option(item), item)
+             }
+         })
         tarchange({value:'All'})
         serchange({value:'All'})
         shipchange({value:'All'})
+        wekchange({value:'All'})
     });
 });
 
@@ -144,6 +160,35 @@ function shipchange(that) {
         });
     } else {
         data.applyFilterAsync("Vessel（船名）", [that.value, 'Null'], "replace", { isExcludeMode: false })
+        console.log('ship change=>', that.value)
+    }
+
+}
+function wekchange(that) {
+    var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
+    if (that.value === 'All') {
+        tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图").getDataSourcesAsync().then(datasources => {
+            var dataSource = datasources.find(datasource => datasource.name === "仓库+ (宏观航运全局New)");
+            return dataSource.getLogicalTablesAsync().then((logicalTables) => {
+                console.log('nihao=>', logicalTables)
+                return dataSource.getLogicalTableDataAsync(logicalTables[6].id)//船舶表
+            });
+        }).then(dataTable => {
+            console.log('dataTable=>', dataTable.columns)
+            //筛选出week
+            let fieldA = dataTable.columns.find(column => column.fieldName === "etd_weeks");
+            var listA = [];
+            for (let row of dataTable.data) {
+                listA.push(row[fieldA.index].value);
+            }
+            let valuesA = listA.filter((el, i, arr) => arr.indexOf(el) === i);
+            valuesA.push('Null')
+            var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
+            data.applyFilterAsync('etd_weeks', valuesA, "replace", { isExcludeMode: false })
+            console.log('etd_weeks', valuesA)
+        });
+    } else {
+        data.applyFilterAsync("etd_weeks", [that.value, 'Null'], "replace", { isExcludeMode: false })
         console.log('ship change=>', that.value)
     }
 
