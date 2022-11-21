4b63fe632e8b8679662a982fbcc5f2f22b947f34
Jo Yang <joyang@datagile.cn>
2022-11-21 21:12:56 +0800

    1

--- Samples/DateItem/DateItem.js
+++ Samples/DateItem/DateItem.js
@@ -1,7 +1,16 @@
 tableau.extensions.initializeAsync().then(function () {
     var timeDate = new Date().toLocaleDateString()
-    document.getElementById('timeA').innerHTML ='Up to date:'+ timeDate.replace(/\//g, '-')
-
-}); 
-
-
+    var list = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
+    var hour = new Date().getHours()
+    var data = '00'
+    if (hour < 2 || hour > 22) {
+        data = '00'
+    } else {
+        var i = 0
+        while (hour >= list[i]) {
+            i++;
+        }
+        data = list[i - 1]
+    }
+    document.getElementById('timeA').innerHTML = 'Up to date:' + timeDate.replace(/\//g, '-') +' '+`${data}:00:00`
+});
--- Samples/HeadRoute/HeadRoute.js
+++ Samples/HeadRoute/HeadRoute.js
@@ -2,8 +2,19 @@
 
 tableau.extensions.initializeAsync().then(function () {
     var timeDate = new Date().toLocaleDateString()
-    document.getElementById('timeA').innerHTML ='Up to date:'+ timeDate.replace(/\//g, '-')
-   
+    var list = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
+    var hour = new Date().getHours()
+    var data = '00'
+    if (hour < 2 || hour > 22) {
+        data = '00'
+    } else {
+        var i = 0
+        while (hour >= list[i]) {
+            i++;
+        }
+        data = list[i - 1]
+    }
+    document.getElementById('timeA').innerHTML = 'Up to date:' + timeDate.replace(/\//g, '-') +' '+`${data}:00:00`   
 });
 function goChina(){
     window.parent.frames.location.href="https://10.192.112.83/#/views/KPI3_0/sheet6_1"
