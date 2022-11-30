d1f81556df6b8724432d55600e8275218f43b600
Jo Yang <joyang@datagile.cn>
2022-11-30 09:37:58 +0800

    中国地图-special monitoring

--- Samples/FootBar/FootBar.js
+++ Samples/FootBar/FootBar.js
@@ -2,47 +2,47 @@
 
 tableau.extensions.initializeAsync().then(function () {
     //Add your JavaScript code here to call the Extensions API
-    var dataList = ['alinCheck', 'lcCheck', 'warCheck','supCheck']
+    var dataList = ['alinCheck', 'lcCheck', 'warCheck', 'supCheck', 'spaCheck']
     dataList.forEach(item => {
         document.getElementById(item).checked = true
     })
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "中国地图正式版UAT")
-    data.applyFilterAsync("Type", [1, 2, 3,4, 'Null'], "replace", {
+    data.applyFilterAsync("Type", [1, 2, 3, 4, 7, 'Null'], "replace", {
         isExcludeMode: false
     })
 });
 
 function alinCheck(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "中国地图正式版UAT")
-    data.applyFilterAsync("Type", [1], that.checked?"add":"remove", {
+    data.applyFilterAsync("Type", [1], that.checked ? "add" : "remove", {
         isExcludeMode: false
     })
 }
 
 function lcCheck(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "中国地图正式版UAT")
-    data.applyFilterAsync("Type", [2], that.checked?"add":"remove", {
+    data.applyFilterAsync("Type", [2], that.checked ? "add" : "remove", {
         isExcludeMode: false
     })
 }
 
 function warCheck(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "中国地图正式版UAT")
-    data.applyFilterAsync("Type", [3], that.checked?"add":"remove", {
+    data.applyFilterAsync("Type", [3], that.checked ? "add" : "remove", {
         isExcludeMode: false
     })
 }
 
 function supCheck(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "中国地图正式版UAT")
-    data.applyFilterAsync("Type", [4], that.checked?"add":"remove", {
+    data.applyFilterAsync("Type", [4], that.checked ? "add" : "remove", {
         isExcludeMode: false
     })
 }
 
 function spaCheck(that) {
     var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "中国地图正式版UAT")
-    data.applyFilterAsync("Type", [7], that.checked?"add":"remove", {
+    data.applyFilterAsync("Type", [7], that.checked ? "add" : "remove", {
         isExcludeMode: false
     })
 }
