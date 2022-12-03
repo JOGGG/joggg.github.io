ca4aacee9e7d2d40eec59cb5974aa8e3cb6bfd3a
Jo Yang <joyang@datagile.cn>
2022-12-03 17:18:58 +0800

    。

--- Samples/FootBarForm/FootBarForm.js
+++ Samples/FootBarForm/FootBarForm.js
@@ -8,7 +8,7 @@ tableau.extensions.initializeAsync().then(function () {
     // var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "宏观航运图")
     // data.applyFilterAsync("Type (Factoryinfo)", [10, 'Null'], "replace", {
     //     isExcludeMode: false
-    // })
+    // })  
 
     console.log('---Footbar form initializeAsync------')
     tableau.extensions.dashboardContent.dashboard.worksheets.forEach(item => {
@@ -34,13 +34,179 @@ tableau.extensions.initializeAsync().then(function () {
         console.log('----Listener start------', selectionEvent)
         //{
 
-        calcOnFilterChange();
+        try {
+            calcOnFilterChange();
+        } catch (error) {
+
+        }
 
 
     });
+    tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "船舶航线明细表").getDataSourcesAsync().then(datasources => {
+        var dataSource = datasources.find(datasource => datasource.name === "仓库+ (宏观航运全局New)");
+        return dataSource.getLogicalTablesAsync().then((logicalTables) => {
+            console.log('nihao123=>', logicalTables)
+            var lgTabel = logicalTables.find(item => {
+                return item.caption === '船舶'
+            })
+            console.log(lgTabel)
+            return dataSource.getLogicalTableDataAsync(lgTabel.id) //船舶表
+        });
+    }).then(dataTable => {
+        // let result = [];
+        console.log("---------calcOnFilterChange 开始获取船舶明细表的筛选器-------------");
+        //获取筛选器的值
+        var data = tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "船舶航线明细表")
+        data.getFiltersAsync().then(res => {
+            console.log(res)
+            //serviceline
+            var fvServiceLine = res.find(item => {
+                return item.fieldName == 'Service Line'
+            })
+            var List = []
+            if (fvServiceLine.length) {
+                fvServiceLine.appliedValues.forEach(item => {
+                    List.push(item.value)
+                })
+
+            }
+            var valServiceLine = List[0];
+            console.log("---------serviceLine:" + valServiceLine)
 
 
-    calcOnFilterChange();
+            //Pol
+            var fvPol = res.find(item => {
+                return item.fieldName == 'pol (DWS Vesselinfo)'
+            })
+            var listPol = []
+            var valPol = "";
+            console.log("---------fvPol:", +fvPol)
+            if (fvPol.length) {
+                fvPol.appliedValues.forEach(item => {
+                    listPol.push(item.value)
+                })
+                valPol = listPol[0];
+                console.log("---------pol:" + valPol)
+            }
+
+
+            //Pod
+            var fvPod = res.find(item => {
+                return item.fieldName == 'pod (DWS Vesselinfo)'
+            })
+            var listPod = []
+            var valPod = "";
+            console.log("---------fvPod:" + fvPod)
+            if (fvPod.length) {
+                fvPod.appliedValues.forEach(item => {
+                    listPod.push(item.value)
+                })
+                valPod = listPod[0];
+                console.log("---------pod:" + valPod)
+            }
+
+
+            //Etd Weeks
+            var fvWeek = res.find(item => {
+                return item.fieldName == 'Etd Weeks'
+            })
+            var listWeek = []
+            var valWeek = "";
+            console.log("---------fvWeek:" + fvWeek)
+            if (fvWeek.length) {
+                fvWeek.appliedValues.forEach(item => {
+                    listWeek.push(item.value)
+                })
+                valWeek = listWeek[0];
+                console.log("---------Weeks:" + valWeek)
+            }
+
+            console.log("---------获取船舶明细表的筛选器结束-------------");
+
+            console.log("---------开始筛选-------------");
+            let fieldServiceLine = dataTable.columns.find(column => column.fieldName === "Service Line");
+            let fieldPol = dataTable.columns.find(column => column.fieldName === "pol (DWS Vesselinfo)");
+            let fieldPod = dataTable.columns.find(column => column.fieldName === "pod (DWS Vesselinfo)");
+            let fieldWeek = dataTable.columns.find(column => column.fieldName === "Etd Weeks");
+            let fieldTag = dataTable.columns.find(column => column.fieldName === "Layer_Tag");
+            let fieldShip = dataTable.columns.find(column => column.fieldName === "Ship direction");
+            let fieldPodeta = dataTable.columns.find(column => column.fieldName === "Podeta Status");
+            let fieldPoletd = dataTable.columns.find(column => column.fieldName === "Poletd Status");
+            console.log(fieldPoletd);
+            console.log(fieldPodeta);
+            filterData = dataTable.data;
+            console.log("为筛选前数量：" + filterData.length);
+            if (valServiceLine) {
+                console.log("===valServiceLine:" + valServiceLine)
+                filterData = filterData.filter(item => {
+                    return (item[fieldServiceLine.index].value == valServiceLine)
+                })
+                console.log("ServiceLine筛选后数量：" + filterData.length);
+            }
+
+            if (valPol) {
+                console.log("===valPol:" + valPol)
+                filterData = filterData.filter(item => {
+                    return (item[fieldPol.index].value == valPol)
+                })
+                console.log("Pol筛选后数量：" + filterData.length);
+            }
+
+            if (valPod) {
+                console.log("===valPod:" + valPod)
+                filterData = filterData.filter(item => {
+                    return (item[fieldPod.index].value == valPod)
+                })
+                console.log("Pod筛选后数量：" + filterData.length);
+            }
+
+            if (valWeek) {
+                console.log("===valWeek:" + valWeek)
+                filterData = filterData.filter(item => {
+                    return (item[fieldWeek.index].value == valWeek)
+                })
+                console.log("Week筛选后数量：" + filterData.length);
+            }
+            filterData = filterData.filter(item => {
+                return (item[fieldTag.index].value == 1)
+            })
+
+            console.log("---------筛选结束-------------");
+            console.log(filterData.length);
+            console.log(filterData);
+
+            var rNormal = 0
+            var rDelay = 0
+            var rCancel = 0
+            console.log(document.getElementById('normal'))
+
+
+            filterData.forEach(item => {
+                console.log(rNormal, rDelay, rCancel)
+                console.log(item[fieldShip.index].value, item[fieldPoletd.index].value, item[fieldPodeta.index].value)
+                if (item[fieldShip.index].value == 'I' && item[fieldPoletd.index].value == '1') {
+                    rNormal = rNormal + 1
+                } else if (item[fieldShip.index].value == 'E' && item[fieldPodeta.index].value == '1') {
+                    rNormal = rNormal + 1
+                } else if (item[fieldShip.index].value == 'I' && item[fieldPoletd.index].value == '2') {
+                    rDelay = rDelay + 1
+                } else if (item[fieldShip.index].value == 'E' && item[fieldPodeta.index].value == '2') {
+                    rDelay = rDelay + 1
+                } else if (item[fieldShip.index].value == 'I' && item[fieldPoletd.index].value == '3') {
+                    rCancel = rCancel + 1
+                } else if (item[fieldShip.index].value == 'E' && item[fieldPodeta.index].value == '3') {
+                    rCancel = rCancel + 1
+                }
+            })
+            console.log(rNormal, rDelay, rCancel)
+            document.getElementById('normal').innerText = rNormal;
+            document.getElementById('delay').innerText = rDelay;
+            document.getElementById('cancelled').innerText = rCancel;
+        });
+
+
+
+    })
 });
 
 function onParameterChange(parameterChangeEvent) {
@@ -154,6 +320,8 @@ function calcOnFilterChange() {
             let fieldPol = dataTable.columns.find(column => column.fieldName === "pol (DWS Vesselinfo)");
             let fieldPod = dataTable.columns.find(column => column.fieldName === "pod (DWS Vesselinfo)");
             let fieldWeek = dataTable.columns.find(column => column.fieldName === "Etd Weeks");
+            let fieldTag = dataTable.columns.find(column => column.fieldName === "Layer_Tag");
+            let fieldShip = dataTable.columns.find(column => column.fieldName === "Ship direction");
             let fieldPodeta = dataTable.columns.find(column => column.fieldName === "Podeta Status");
             let fieldPoletd = dataTable.columns.find(column => column.fieldName === "Poletd Status");
             let fieldPoleta = dataTable.columns.find(column => column.fieldName === "Poleta Status");
@@ -195,91 +363,128 @@ function calcOnFilterChange() {
                 })
                 console.log("Week筛选后数量：" + filterData.length);
             }
+            filterData = filterData.filter(item => {
+                return (item[fieldTag.index].value == 1)
+            })
 
             console.log("---------筛选结束-------------");
             console.log(filterData.length);
             console.log(filterData);
-            tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "船舶航线明细表").getParametersAsync().then(function (parameters) {
 
-                var delayDetec, delayThreshold;
+            var rNormal = 0
+            var rDelay = 0
+            var rCancel = 0
+            console.log(document.getElementById('normal'))
 
-                parameters.forEach(function (p) {
-                    if (p.name === "Delay Detec by") {
-                        delayDetec = p.currentValue.value;
-                    }
+            document.getElementById('normal').innerText = rNormal;
+            document.getElementById('delay').innerText = rDelay;
+            document.getElementById('cancelled').innerText = rCancel;
 
-                    if (p.name === "Delay Threshold") {
-                        delayThreshold = p.currentValue.value;
-                    }
-                });
-
-
-                // ETD(POL)
-                // ETA(POL)
-                // && p.currentValue.value==="ETA(POD)"
-
-                console.log('---Delay Detec by:' + delayDetec);
-                console.log('---Delay Threshold:' + delayThreshold);
-
-
-                console.log("<---------参数筛选前-------------");
-                console.log("---------数量-------------:" + filterData.length);
-                console.log(filterData);
-
-                var rowsNormal = []
-                var rowsDelay = []
-                switch (delayDetec) {
-                    case "ETD(POL)":
-                        console.log("<---------ETD(POL)-------------");
-                        rowsNormal = filterData.filter(item => {
-                            return (item[fieldPoletd.index].value != 3 && item[fieldPoletdDelay.index].value - delayThreshold <= 0)
-                        })
-                        rowsDelay = filterData.filter(item => {
-                            return (item[fieldPoletd.index].value != 3 && item[fieldPoletdDelay.index].value - delayThreshold > 0)
-                        })
-                        console.log("---------normal-------------:" + rowsNormal.length);
-                        console.log(rowsNormal);
-                        console.log("---------Delay-------------:" + rowsDelay.length);
-                        console.log(rowsDelay);
-                        console.log("---------ETD(POL)------------->");
-                        break;
-                    case "ETA(POL)":
-                        console.log("<---------ETA(POL)-------------");
-                        rowsNormal = filterData.filter(item => {
-                            return (item[fieldPoleta.index].value != 3 && item[fieldPoletaDelay.index].value - delayThreshold <= 0)
-                        })
-                        rowsDelay = filterData.filter(item => {
-                            return (item[fieldPoleta.index].value != 3 && item[fieldPoletaDelay.index].value - delayThreshold > 0)
-                        })
-                        console.log("---------normal-------------:" + rowsNormal.length);
-                        console.log(rowsNormal);
-                        console.log("---------Delay-------------:" + rowsDelay.length);
-                        console.log(rowsDelay);
-                        console.log("---------ETA(POL)------------->");
-                        break;
-                    case "ETA(POD)":
-                        console.log("<---------ETA(POD)-------------");
-                        rowsNormal = filterData.filter(item => {
-                            return (item[fieldPodeta.index].value != 3 && item[fieldPodetaDelay.index].value - delayThreshold <= 0)
-                        })
-                        rowsDelay = filterData.filter(item => {
-                            return (item[fieldPodeta.index].value != 3 && item[fieldPodetaDelay.index].value - delayThreshold > 0)
-                        })
-                        console.log("---------normal-------------:" + rowsNormal.length);
-                        console.log(rowsNormal);
-                        console.log("---------Delay-------------:" + rowsDelay.length);
-                        console.log(rowsDelay);
-                        console.log("---------ETA(POD)------------->");
-                        break;
+            filterData.forEach(item => {
+                console.log(rNormal, rDelay, rCancel)
+                console.log(item[fieldShip.index].value, item[fieldPoletd.index].value, item[fieldPodeta.index].value)
+                if (item[fieldShip.index].value == 'I' && item[fieldPoletd.index].value == '1') {
+                    rNormal = rNormal + 1
+                } else if (item[fieldShip.index].value == 'E' && item[fieldPodeta.index].value == '1') {
+                    rNormal = rNormal + 1
+                } else if (item[fieldShip.index].value == 'I' && item[fieldPoletd.index].value == '2') {
+                    rDelay = rDelay + 1
+                } else if (item[fieldShip.index].value == 'E' && item[fieldPodeta.index].value == '2') {
+                    rDelay = rDelay + 1
+                } else if (item[fieldShip.index].value == 'I' && item[fieldPoletd.index].value == '3') {
+                    rCancel = rCancel + 1
+                } else if (item[fieldShip.index].value == 'E' && item[fieldPodeta.index].value == '3') {
+                    rCancel = rCancel + 1
                 }
+            })
+            console.log(rNormal, rDelay, rCancel)
 
-                document.getElementById('normal').innerText = rowsNormal.length;
-                document.getElementById('delay').innerText = rowsDelay.length;
-                let rowsCancel = filterData.filter(item => {
-                    return (item[fieldPodeta.index].value == 3 || item[fieldPoletd.index].value == 3)
-                })
-                document.getElementById('cancelled').innerText = rowsCancel.length;
-            });
+
+            // let rowsCancel = filterData.filter(item => {
+            //     return (item[fieldPodeta.index].value == 3 || item[fieldPoletd.index].value == 3)
+            // })
+
+            // tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "船舶航线明细表").getParametersAsync().then(function (parameters) {
+
+            //     var delayDetec, delayThreshold;
+
+            //     parameters.forEach(function (p) {
+            //         if (p.name === "Delay Detec by") {
+            //             delayDetec = p.currentValue.value;
+            //         }
+
+            //         if (p.name === "Delay Threshold") {
+            //             delayThreshold = p.currentValue.value;
+            //         }
+            //     });
+
+
+            //     // ETD(POL)
+            //     // ETA(POL)
+            //     // && p.currentValue.value==="ETA(POD)"
+
+            //     console.log('---Delay Detec by:' + delayDetec);
+            //     console.log('---Delay Threshold:' + delayThreshold);
+
+
+            //     console.log("<---------参数筛选前-------------");
+            //     console.log("---------数量-------------:" + filterData.length);
+            //     console.log(filterData);
+
+            //     var rowsNormal = []
+            //     var rowsDelay = []
+            // switch (delayDetec) {
+            //     case "ETD(POL)":
+            //         console.log("<---------ETD(POL)-------------");
+            //         rowsNormal = filterData.filter(item => {
+            //             return (item[fieldPoletd.index].value != 3 && item[fieldPoletdDelay.index].value - delayThreshold <= 0)
+            //         })
+            //         rowsDelay = filterData.filter(item => {
+            //             return (item[fieldPoletd.index].value != 3 && item[fieldPoletdDelay.index].value - delayThreshold > 0)
+            //         })
+            //         console.log("---------normal-------------:" + rowsNormal.length);
+            //         console.log(rowsNormal);
+            //         console.log("---------Delay-------------:" + rowsDelay.length);
+            //         console.log(rowsDelay);
+            //         console.log("---------ETD(POL)------------->");
+            //         break;
+            //     case "ETA(POL)":
+            //         console.log("<---------ETA(POL)-------------");
+            //         rowsNormal = filterData.filter(item => {
+            //             return (item[fieldPoleta.index].value != 3 && item[fieldPoletaDelay.index].value - delayThreshold <= 0)
+            //         })
+            //         rowsDelay = filterData.filter(item => {
+            //             return (item[fieldPoleta.index].value != 3 && item[fieldPoletaDelay.index].value - delayThreshold > 0)
+            //         })
+            //         console.log("---------normal-------------:" + rowsNormal.length);
+            //         console.log(rowsNormal);
+            //         console.log("---------Delay-------------:" + rowsDelay.length);
+            //         console.log(rowsDelay);
+            //         console.log("---------ETA(POL)------------->");
+            //         break;
+            //     case "ETA(POD)":
+            //         console.log("<---------ETA(POD)-------------");
+            //         rowsNormal = filterData.filter(item => {
+            //             return (item[fieldPodeta.index].value != 3 && item[fieldPodetaDelay.index].value - delayThreshold <= 0)
+            //         })
+            //         rowsDelay = filterData.filter(item => {
+            //             return (item[fieldPodeta.index].value != 3 && item[fieldPodetaDelay.index].value - delayThreshold > 0)
+            //         })
+            //         console.log("---------normal-------------:" + rowsNormal.length);
+            //         console.log(rowsNormal);
+            //         console.log("---------Delay-------------:" + rowsDelay.length);
+            //         console.log(rowsDelay);
+            //         console.log("---------ETA(POD)------------->");
+            //         break;
+            // }
+
+            //     document.getElementById('normal').innerText = rowsNormal.length;
+            //     document.getElementById('delay').innerText = rowsDelay.length;
+            //     let rowsCancel = filterData.filter(item => {
+            //         return (item[fieldPodeta.index].value == 3 || item[fieldPoletd.index].value == 3)
+            //     })
+            //     document.getElementById('cancelled').innerText = rowsCancel.length;
+            // });
 
 
 
